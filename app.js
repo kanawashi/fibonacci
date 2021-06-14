6ff0d8467bb446d25a0ccc207f85013a4baa2ab7
kinoa kanawashi <kanawashi@kinoanoAir.elecom>
2021-06-15 00:10:10 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 'use strict';
+/**
+ * n 番目のフィボナッチ数を返す　fib関数
+ * @param {Number} n 
+ * @returns {Number}
+ */
+function fib(n){
+    if (n === 0){
+        return n;
+    }else if (n === 1){
+        return 1;
+    }else{
+        return fib(n-2) + fib(n-1);
+    }
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++){
+    console.log(fib(i));
+}
